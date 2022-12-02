b59a5dd240cbe59dda48d66ebfa9d1f40b0a233c
Konstantin Mechik <konstantinmechik@yandex.ru>
2022-12-02 20:45:36 +0300

    add README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # qa_python
+# тест на добавление двух книг
+test_add_new_book_two_books_added_books
+# тест на добавление уже добавленной ранее книги
+test_add_new_book_existing_book_not_added
+# тест на установку рейтинга добавленной книги
+test_set_book_rating_new_value_in_range_1_to_10_set_new_rating
+# тест на установку рейтинга выше 10
+test_set_book_rating_new_value_not_in_range_1_to_10_not_set_new_rating
+# тест на проверку стандартного рейтинга добавленной книги
+test_get_book_rating_name_book_shows_rating_1
+# тест на вывод списка с определённым рейтингом
+test_get_books_with_specific_rating_value_in_range_1_to_10_show_list
+# тест на вывод путого списка при запросе рейтинга 11
+test_get_books_with_specific_rating_value_not_in_range_1_to_10_show_empty_list
+# тест на получение всего словаря с книгами и рейтингом
+test_get_books_rating_show_dict
+# тест добавление книги в избранное из добавленных раньше
+test_add_book_in_favorites_book_from_books_rating_add_book_in_favorites
+# тест добавление новой книги сразу в избранное
+test_add_book_in_favorites_book_not_from_books_rating_not_add_book_in_favorites
+# тест на удаление книги из избранного
+test_delete_book_from_favorites_book_from_favorites_book_not_in_favorites
--- tests.py
+++ tests.py
@@ -1,58 +1,66 @@
 
 class TestBooksCollector:
     # тест на добавление двух книг
-    def test_add_new_book_add_two_books(self, collector_with_book):
+    def test_add_new_book_two_books_added_books(self, collector_with_book):
         collector_with_book.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector_with_book.get_books_rating()) == 2
 
     # тест на добавление уже добавленной ранее книги
-    def test_add_new_book_try_added_book(self, collector_with_book):
+    def test_add_new_book_existing_book_not_added(self, collector_with_book):
         collector_with_book.add_new_book('Книга')
         assert len(collector_with_book.get_books_rating()) == 1
 
     # тест на установку рейтинга добавленной книги
-    def test_set_book_rating_set_rating_10(self, collector_with_book):
+    def test_set_book_rating_new_value_in_range_1_to_10_set_new_rating(self, collector_with_book):
         collector_with_book.set_book_rating('Книга', 10)
         assert collector_with_book.get_book_rating('Книга') == 10
 
-    def test_set_book_rating_try_set_rating_11_past_value_1(self, collector_with_book): # тест на установку рейтинга выше 10
+    # тест на установку рейтинга выше 10
+    def test_set_book_rating_new_value_not_in_range_1_to_10_not_set_new_rating(self, collector_with_book):
         collector_with_book.set_book_rating('Книга', 11)
         assert collector_with_book.get_book_rating('Книга') == 1
 
-    def test_get_book_rating_new_book_rating_1(self, collector_with_book): # тест на проверку стандартного рейтинга добавленной книги
+    # тест на проверку стандартного рейтинга добавленной книги
+    def test_get_book_rating_name_book_shows_rating_1(self, collector_with_book):
         assert collector_with_book.get_book_rating('Книга') == 1
 
-    def test_get_books_with_specific_rating_show_list_with_rating_9(self, collector_with_book): # тест на вывод списка с определённым рейтингом
+    # тест на вывод списка с определённым рейтингом
+    def test_get_books_with_specific_rating_value_in_range_1_to_10_show_list(self, collector_with_book):
         collector_with_book.add_new_book('Доширак - лучший выбор!')
         collector_with_book.set_book_rating('Доширак - лучший выбор!', 9)
         collector_with_book.add_new_book('Мисс говядина')
         collector_with_book.set_book_rating('Мисс говядина', 9)
         assert collector_with_book.get_books_with_specific_rating(9) == ['Доширак - лучший выбор!', 'Мисс говядина']
 
-    def test_get_books_with_specific_rating_show_list_with_rating_11(self, collector_with_book): # тест на вывод путого списка при запросе рейтинга 11
+    # тест на вывод путого списка при запросе рейтинга 11
+    def test_get_books_with_specific_rating_value_not_in_range_1_to_10_show_empty_list(self, collector_with_book):
         collector_with_book.add_new_book('Доширак - лучший выбор!')
         collector_with_book.set_book_rating('Доширак - лучший выбор!', 9)
         collector_with_book.add_new_book('Мисс говядина')
         collector_with_book.set_book_rating('Мисс говядина', 9)
         assert collector_with_book.get_books_with_specific_rating(11) == []
 
-    def test_get_books_rating_show_dict(self, collector_with_book): # тест на получение всего словаря с книгами и рейтингом
+    # тест на получение всего словаря с книгами и рейтингом
+    def test_get_books_rating_show_dict(self, collector_with_book):
         collector_with_book.add_new_book('Доширак - лучший выбор!')
         collector_with_book.set_book_rating('Доширак - лучший выбор!', 9)
         collector_with_book.add_new_book('Мисс говядина')
         collector_with_book.set_book_rating('Мисс говядина', 9)
         assert collector_with_book.get_books_rating() == {'Книга': 1, 'Доширак - лучший выбор!': 9, 'Мисс говядина': 9}
 
-    def test_add_book_in_favorites_add_book(self, collector_with_book): # тест добавление книги в избранное из словаря
+    # тест добавление книги в избранное из добавленных раньше
+    def test_add_book_in_favorites_book_from_books_rating_add_book_in_favorites(self, collector_with_book):
         collector_with_book.add_new_book('Терминатор Кроль: Восстание морковок')
         collector_with_book.add_book_in_favorites('Терминатор Кроль: Восстание морковок')
         assert collector_with_book.get_list_of_favorites_books() == ['Терминатор Кроль: Восстание морковок']
 
-    def test_add_book_in_favorites_add_not_existing_book(self, collector_with_book): # тест добавление новой книги сразу в избранное
+    # тест добавление новой книги сразу в избранное
+    def test_add_book_in_favorites_book_not_from_books_rating_not_add_book_in_favorites(self, collector_with_book):
         collector_with_book.add_book_in_favorites('Терминатор Кроль: Восстание морковок')
         assert collector_with_book.get_list_of_favorites_books() == []
 
-    def test_delete_book_from_favorites_delete_success(self, collector_with_book): # тест на удаление книги из избранного
+    # тест на удаление книги из избранного
+    def test_delete_book_from_favorites_book_from_favorites_book_not_in_favorites(self, collector_with_book):
         collector_with_book.add_new_book('Терминатор Кроль: Восстание морковок')
         collector_with_book.add_book_in_favorites('Терминатор Кроль: Восстание морковок')
         collector_with_book.delete_book_from_favorites('Терминатор Кроль: Восстание морковок')
