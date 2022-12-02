dfaed9c6d8ddb9eccc7290c9453832e9c0326709
Konstantin Mechik <konstantinmechik@yandex.ru>
2022-12-02 17:09:31 +0300

    --

--- tests.py
+++ tests.py
@@ -1,7 +1,5 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
     def test_add_new_book_add_two_books(self, collector_with_book): # тест на добавление двух книг
@@ -24,12 +22,18 @@ class TestBooksCollector:
         assert collector_with_book.get_book_rating('Книга') == 1
 
     def test_get_books_with_specific_rating_show_list_with_rating_9(self, collector_with_book): # тест на вывод списка с определённым рейтингом
-        collector_with_book.set_book_rating('Книга', 10)
         collector_with_book.add_new_book('Доширак - лучший выбор!')
         collector_with_book.set_book_rating('Доширак - лучший выбор!', 9)
         collector_with_book.add_new_book('Мисс говядина')
         collector_with_book.set_book_rating('Мисс говядина', 9)
         assert collector_with_book.get_books_with_specific_rating(9) == ['Доширак - лучший выбор!', 'Мисс говядина']
+
+    def test_get_books_with_specific_rating_show_list_with_rating_11(self, collector_with_book): # тест на вывод путого списка при запросе рейтинга 11
+        collector_with_book.add_new_book('Доширак - лучший выбор!')
+        collector_with_book.set_book_rating('Доширак - лучший выбор!', 9)
+        collector_with_book.add_new_book('Мисс говядина')
+        collector_with_book.set_book_rating('Мисс говядина', 9)
+        assert collector_with_book.get_books_with_specific_rating(11) == []
 
     def test_get_books_rating_show_dict(self, collector_with_book): # тест на получение всего словаря с книгами и рейтингом
         collector_with_book.set_book_rating('Книга', 10)
